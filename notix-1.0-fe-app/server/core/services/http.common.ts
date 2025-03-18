7cac02288357900275484031e99acd9f617f4e28
Leonardo Vargas <leonardo.vargas@example.com>
2025-03-18 02:32:32 -0500

    feat

--- notix-1.0-fe-app/server/core/services/http.common.ts
+++ notix-1.0-fe-app/server/core/services/http.common.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://192.168.1.36:7005/api/v0'
+const baseUrl = 'https://localhost:7005/api/v0'
 
 export default axios.create({
   baseURL: baseUrl,
