6f8e0a5b1d3af983e2492a0925e98e168015fa9f
Leonardo Vargas <leonardo.vargas@example.com>
2025-03-22 18:55:14 -0500

    fix: erase configuration

--- backend/obj/Debug/net8.0/Notix.Beta.API.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Notix.Beta.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notix.Beta.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857e410f09f75cfc9f6d5556ed74a8aa88066051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcba398ff98a962b9a4d12e7b26896d0cbaf1c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notix.Beta.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notix.Beta.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
